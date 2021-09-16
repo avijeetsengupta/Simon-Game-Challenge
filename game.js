bdedefca046dafd623ce11938ee2c3b1a018080b
avijeetsengupta <avijeetfly143@gmail.com>
2021-09-16 16:50:19 +0530

    changes

--- game.js
+++ game.js
@@ -1,69 +1,91 @@
-// randomly select any colour.
+
 var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPattern = [];
-var level = 0;
+
 var started = false;
+var level = 0;
 
-$(document).keypress(function(){
-    if(!started){
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
-        $("#level-title").text("level " + level);
-        nextSequence();
-        started = true;
-    }
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
 });
 
 
-// To check which button got clicked.
-$(".btn").click(function()  {
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
+function checkAnswer(currentLevel) {
 
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
-    checkAnswer(userClickedPattern.length-1);
-} );
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
-function checkAnswer(currentLevel){
-        if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
-            console.log("sucsess");
-        }else{
-            console.log("wrong");
-        }
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+
+    } else {
+
+      console.log("wrong");
+
+      playSound("wrong");
+
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      startOver();
+    }
+
 }
-
 
 function nextSequence() {
 
-    level++;
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
 
-    $("#level-title").text("Level " + level);
-    var randomNumber = Math.floor(Math.random() * 4)
-    var randomChosenColour = buttonColours[randomNumber]; 
-    gamePattern.push(randomChosenColour);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
-// This think works when you press any key basically blinks at stratting. 
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    
-// To make a sound when pressing color.
-    playSound(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 }
 
-// To match the sound effect from btn to next sequence
 function playSound(name) {
-    var audio = new Audio("sounds/"+ name + ".mp3");
-    audio.play();
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
 
-// To show flash in a button.  
-function animatePress(currentColour){
-         
-    $("#"+currentColour).addClass("pressed");
+function startOver() {
 
-    setTimeout(function() {
-        $("#"+currentColour).removeClass("pressed")
-    },100)
-
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
